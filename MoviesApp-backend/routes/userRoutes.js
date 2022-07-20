82a1b45f9670a12b8da83dbec8c965881ba4c444
Shivakumar M <shivakumarm250@gmail.com>
2022-07-20 13:21:39 +0530

    added verify token middleware

--- MoviesApp-backend/routes/userRoutes.js
+++ MoviesApp-backend/routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
+const auth = require('../auth/verifyToken');
 const router = express.Router();
 
 router.route('/login').post(userController.loginUser);
 
 router.route('/register').post(userController.registerUser);
 
-router.route('/movies/:trend/:page').get(userController.tmdbGetTrendingMovies);
+router.route('/movies/:trend/:page').get(auth, userController.tmdbGetTrendingMovies);
 
-router.route('/people/:page').get(userController.tmdbGetTrendingPeople);
+router.route('/people/:page').get(auth, userController.tmdbGetTrendingPeople);
 
-router.route('/search/:query').get(userController.tmdbGetSearchResults);
+router.route('/search/:query').get(auth, userController.tmdbGetSearchResults);
 
 module.exports = router;
